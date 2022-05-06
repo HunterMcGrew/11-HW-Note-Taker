9efeb67658b223241ae7a4c39429fb2acdad0218
HunterLM <hunter109@gmail.com>
2022-05-05 23:29:15 -0500

    make it better

--- routes/api/noteRoutes.js
+++ routes/api/noteRoutes.js
@@ -31,6 +31,7 @@ router.post("/", async (req, res) => {
 router.delete("/:id", async (req, res) => {
     const updateNote = await Note.update(
         {
+            id: req.body.id,
             title: req.body.title,
             text: req.body.text
         },
